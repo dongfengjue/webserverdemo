0e06c1c0bf05381f2c30a9fe4a9cd30beb0660c0
dongfengjue <1009529808@qq.com>
2019-09-19 16:12:05 +0800

    手写树

---
+++ src/main/java/com/chenbing/tree/NormalTree/Node.java
@@ -0,0 +1,34 @@
+package com.chenbing.tree.NormalTree;
+
+public class Node {
+
+        private Node left;
+
+        private Node right;
+
+        private Object data;
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public Object getData() {
+            return data;
+        }
+
+        public void setData(Object data) {
+            this.data = data;
+        }
+}
