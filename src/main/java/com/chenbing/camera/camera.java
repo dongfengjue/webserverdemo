c61256ddd37eb6a10593d6f8273db16a2358fd81
dongfengjue <1009529808@qq.com>
2019-10-27 21:02:58 +0800

    增加 开启摄像头代码

---
+++ src/main/java/com/chenbing/camera/camera.java
@@ -0,0 +1,27 @@
+package com.chenbing.camera;
+
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacv.Frame;
+
+import javax.swing.*;
+
+public class camera {
+    public static void main(String args[]) throws Exception {
+        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+        grabber.start();   //开始获取摄像头数据
+        CanvasFrame canvas = new CanvasFrame("摄像头");//新建一个窗口
+        canvas.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        canvas.setAlwaysOnTop(true);
+        while (true) {
+            if (!canvas.isDisplayable()) {//窗口是否关闭
+                grabber.stop();//停止抓取
+                System.exit(-1);//退出
+            }
+
+            Frame frame = grabber.grab();
+
+            canvas.showImage(frame);//获取摄像头图像并放到窗口上显示， 这里的Frame frame=grabber.grab(); frame是一帧视频图像
+            Thread.sleep(10);//50毫秒刷新一次图像
+        }
+    }
+}
